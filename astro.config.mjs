a3947af3d9a75af8898ac4bf108872825d092593
Mystery0 <mystery0dyl520@gmail.com>
2026-05-09 17:43:21 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://pixel-tailor-cn.github.io',
+  site: 'https://pixel.mystery0.vip',
   vite: {
     plugins: [tailwindcss()],
   },
